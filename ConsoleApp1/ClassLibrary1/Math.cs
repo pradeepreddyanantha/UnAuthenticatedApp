975b7472dd95b45f582d45ccf547fd51a449c18f
pradeepreddyanantha <pradeepreddy.anantha@gmail.com>
2021-02-05 11:20:47 +0530

    Un authenticated app sorce code is checked in 2

---
+++ ConsoleApp1/ClassLibrary1/Math.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ConsoleApp1/NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using ClassLibrary1;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test11()
+        {
+            
+            Assert.Pass("Test CAse is PAssed");
+        }
+    }
+}
